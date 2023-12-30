fce6edb0a4ae6a7a070f1a6a5602048afdce000f
Ryotashm <rytshmz021@gmail.com>
2023-12-30 09:50:09 +0900

    とりあえずhomeappから使いそうなファイルを移植。(ビルド通るようにしただけで未編集)

---
+++ swift-2048/DeleteUser.swift
@@ -0,0 +1,7 @@
+import Foundation
+import SwiftUI
+struct DeleteUser: View {
+    var body: some View {
+        Text("退会画面")
+    }
+}
---
+++ swift-2048/iraiEmailConfirmView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+// todo メールを再送するボタンとその処理
+struct iraiEmailConfirmView: View {
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("メールアドレスの確認を行なってください。")
+
+                NavigationLink(destination: loginView()) {
+                    Text("ログイン画面に戻る")
+                }
+            }
+        }
+    }
+}
+
+struct iraiEmailConfirmView_Previews: PreviewProvider {
+    static var previews: some View {
+        iraiEmailConfirmView()
+    }
+}
